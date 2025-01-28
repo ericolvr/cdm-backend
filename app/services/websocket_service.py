56dc6a37fd51d909cbdb14aed7bf20ead1a0c8f6
ericolvr@gmail.com <ericolvr@gmail.com>
2025-01-28 15:58:10 -0300

    websocket manager

---
+++ app/services/websocket_service.py
@@ -0,0 +1,43 @@
+""" Websocket connection manager """
+from typing import Dict
+from fastapi import WebSocket
+
+
+class Manager:
+    """ Connection Manager """
+
+    def __init__(self) -> None:
+        self.active_connections: Dict[str, WebSocket] = {}
+
+
+    async def connect(self, websocket: WebSocket, user: str):
+        """ Connection Method """
+
+        await websocket.accept()
+        self.active_connections[user] = websocket
+
+
+    async def disconnect(self, user: str):
+        """ Disconnect Method """
+
+        if user in self.active_connections:
+            del self.active_connections[user]
+
+    
+    async def send_personal(self, message: str, user: str):
+        """ Send personal message method """
+
+        if user in self.active_connections:
+            await self.active_connections[user].send_text(message)
+
+
+    async def broadcast(self, message: str):
+        """ Broadcast message method """
+
+        for conn in self.active_connections.values():
+            await conn.send_json(message)
+
+
+manager = Manager()
+
+
