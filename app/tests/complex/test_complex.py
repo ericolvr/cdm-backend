95c515ce092468ee9f339bcf46356f8477a73c77
ericolvr@gmail.com <ericolvr@gmail.com>
2025-01-29 07:42:37 -0300

    fyx typo

--- app/tests/complex/test_complex.py
+++ app/tests/complex/test_complex.py
@@ -4,9 +4,11 @@ from app.main import app
 
 client = TestClient(app)
 
+
 @pytest.mark.asyncio
 async def test_create_complex():
     """ Must be create complex and returns 201 """
+    
     response = client.post('/api/v1/complexes/',
         json={
             'name': 'Test Complex',
@@ -16,9 +18,11 @@ async def test_create_complex():
     )
     assert response.status_code == 201
 
+
 @pytest.mark.asyncio
 async def test_get_all_complexes():
     """ Must be return all complexes and returns 200 """
+    
     response = client.get('/api/v1/complexes')
     assert response.status_code == 200
 
@@ -26,6 +30,7 @@ async def test_get_all_complexes():
 @pytest.mark.asyncio
 async def test_get_complex_by_id():
     """ Must be return complex by id and returns 200 """
+    
     response = client.get('/api/v1/complexes/id?id=1')
     assert response.status_code == 200
 
