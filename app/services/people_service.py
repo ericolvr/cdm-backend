1f9919dc95b65b77de89cc11c8375743520b3511
ericolvr@gmail.com <ericolvr@gmail.com>
2025-01-29 07:42:27 -0300

    typo

--- app/services/people_service.py
+++ app/services/people_service.py
@@ -12,75 +12,78 @@ from app.services.token_service import TokenService
 
 
 class PeopleService:
-    """ People Service """
+    """ People service """
     
     def __init__(self, db: Session):
         self.repository = PeopleRepository(db)
         self.database = db
 
 
-    async def create_people(self, people: PeopleCreate, background_tasks: BackgroundTasks):
-        """ Create People """
+    def create_people(self, people: PeopleCreate, background_tasks: BackgroundTasks):
+        """ Create people """
 
         if people.picture:
             picture = self.process_image(people.picture)
         else:
-            picture = ''
+            picture = None 
 
         people = People(
-            complex_id=people.complex_id,
-            apartment_id=people.apartment_id,
             name=people.name,
             document=people.document,
             mobile=people.mobile,
-            picture=picture
+            picture=picture,
+            complex_id=people.complex_id,
+            apartment_id=people.apartment_id
         )
+
+        """
+            Creating a token and send by sms
+        """
         
         background_tasks.add_task(
             TokenService(self.database).create_token, people.mobile
         )
 
-        return await self.repository.create_people(people)
+        return self.repository.create_people(people)
 
 
-    async def get_all_peoples(self):
-        """ Get All People """
+    def get_all_peoples(self):
+        """ Get all peoples """
 
-        peoples = await self.repository.get_all_peoples()
+        peoples = self.repository.get_all_peoples()
         return peoples
 
 
-    async def get_people_by_id(self, id: str):
-        """ Get People By Id """
+    def get_people_by_id(self, id: str):
+        """ Get people by id """
 
-        people = await self.repository.get_people_by_id(id)
+        people = self.repository.get_people_by_id(id)
         return people
     
 
-    async def get_people_by_complex_apartment(self, complex, apartment):
+    def get_people_by_complex_apartment(self, complex, apartment):
         """ Get people complex and apartment """
 
-        peoples = await self.repository.get_people_by_complex_apartment(complex, apartment)
+        peoples = self.repository.get_people_by_complex_apartment(complex, apartment)
         return peoples
     
 
-    async def update_by_id(self, id: int, new_data):
+    def update_by_id(self, id: int, new_data):
         """ Update people by id """
     
-        people = await self.repository.update_by_id(id, new_data)
+        people = self.repository.update_by_id(id, new_data)
         return people
 
 
-    async def process_image(self, picture: str):
+    def process_image(self, picture: str):
         """ Process image """
 
-        # TODO: Should be use a S3 Bucket
-        FOLDER = 'uploads'
+        FOLDER = "uploads"
         os.makedirs(FOLDER, exist_ok=True)
         
         try:
             data = base64.b64decode(picture)
-            name = f'{uuid4()}.png'
+            name = f"{uuid4()}.png"
             path = os.path.join(FOLDER, name)
             
             with open(path, 'wb') as f:
@@ -89,6 +92,4 @@ class PeopleService:
             return picture
         
         except Exception as e:
-            raise HTTPException(
-                status_code=400, detail=f'Error saving image: {str(e)}'
-            )
+            raise HTTPException(status_code=400, detail=f"Error saving image: {str(e)}")
